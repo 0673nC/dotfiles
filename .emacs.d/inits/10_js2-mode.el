33c6888735cd57a201e92930e782a4956f4fe6d6
0673nC <0673nC@0673nC-no-MacBook-Air.local>
2015-04-14 20:28:12 +0900

    fixed js2-mode

--- .emacs.d/inits/10_js2-mode.el
+++ .emacs.d/inits/10_js2-mode.el
@@ -14,19 +14,19 @@
   '(lambda ()
     (add-to-list 'ac-dictionary-files
                  "~/.emacs.d/ac-dict/js2-mode")
-    ;(require 'espresso)
-    (setq espresso-indent-level 2
-          espresso-expr-indent-offset 2
+    (require 'js)
+    (setq js-indent-level 2
+          js-expr-indent-offset 2
           indent-tabs-mode nil)
     (defun my-js-indent-line ()
       (interactive)
       (let* ((parse-status (save-excursion (syntax-ppss (point-at-bol))))
              (offset (- (current-column) (current-indentation)))
-             (indentation (espresso--proper-indentation parse-status)))
+             (indentation (js--proper-indentation parse-status)))
         (back-to-indentation)
         (if (looking-at "case\\s-")
             (indent-line-to (+ indentation 2))
-          (espresso-indent-line))
+          (js-indent-line))
         (when (> offset 0) (forward-char offset))))
     (set (make-local-variable 'indent-line-function) 'my-js-indent-line)
     (define-key js2-mode-map "\C-m" 'newline-and-indent)
