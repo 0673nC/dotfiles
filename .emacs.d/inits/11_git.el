1c8545d39ac8208ba25c7b92bae184521f6bfec4
0673nC <0673nC@users.noreply.github.com>
2016-04-23 14:52:45 +0900

    add magit color setting

--- .emacs.d/inits/11_git.el
+++ .emacs.d/inits/11_git.el
@@ -8,3 +8,18 @@
 (defalias 'gs 'magit-status)
 
 (global-git-gutter-mode t)
+
+;; 文字色変更
+(set-face-foreground 'magit-diff-add "#00FF00")
+(set-face-foreground 'magit-diff-del "#FF0000")
+(set-face-foreground 'magit-diff-file-header "#00FFFF")
+(set-face-foreground 'magit-diff-hunk-header "#00AADD")
+(set-face-foreground 'magit-section-title "#FF00FF")
+
+;; 背景色変更
+(set-face-background 'magit-item-highlight "Black")
+(set-face-background 'magit-diff-add "Black")
+(set-face-background 'magit-diff-del "Black")
+(set-face-background 'magit-diff-file-header "Black")
+(set-face-background 'magit-diff-hunk-header "Black")
+(set-face-background 'magit-section-title "Black")
