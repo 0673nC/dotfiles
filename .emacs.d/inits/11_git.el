99253b8c1f8a20996da3ec5c6c30f692c42c19f6
0673nC <0673nC@users.noreply.github.com>
2018-02-12 16:48:06 +0900

    delete auto-fill-mode settings

--- .emacs.d/inits/11_git.el
+++ .emacs.d/inits/11_git.el
@@ -5,11 +5,6 @@
 (require 'magit)
 (defalias 'gs 'magit-status)
 
-(add-to-list 'with-editor-server-window-alist
-             (cons git-commit-filename-regexp 'switch-to-buffer))
-
-(package-initialize) ; otherwise Emacs doesn't know were to find it
-(setq package-initialize-at-startup nil) ; don't do it again
 (global-git-commit-mode)
 
 (global-git-gutter-mode t)
@@ -23,6 +18,3 @@
 (set-face-foreground 'magit-diff-removed-highlight "#FF0000")
 (set-face-background 'magit-diff-removed-highlight "Black")
 (set-face-background 'magit-diff-lines-boundary "blue")
-
-;; 文字の折り返し無効
-(add-hook 'git-commit-mode-hook (setq auto-fill-mode nil))
