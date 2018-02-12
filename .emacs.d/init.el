e049bfa3f5d75a780527629ac39dd331fab2d327
0673nC <0673nC@users.noreply.github.com>
2018-02-12 16:56:24 +0900

    fix package.el warn

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,13 @@
 ;;================================================
 ;;;  init
 ;;================================================
+
+(require 'package)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
+
 (load "~/.emacs.d/inits/package.el")
 
 (require 'init-loader)
